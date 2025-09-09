bb2b8502ccb701e6696182b8e07da1419441b229
ANujJakhar2104 <anuj.jakhar.2104@gmail.com>
2025-09-10 00:01:08 +0530

    Time: 11 ms (30.16%), Space: 12.4 MB (83.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,3 +1,4 @@
+// simple recursion
 // class Solution {
 // public:
 //     int find(int i, int j, string s1, string s2) {
@@ -20,32 +21,57 @@
 //     }
 // };
 
+// recursion with memoization
+// class Solution {
+// public:
+//     int find(int i, int j, string s1, string s2, vector<vector<int>>& dp) {
+//         if (j == 0)
+//             return i;
+//         if (i == 0)
+//             return j;
+//         if (dp[i][j] != -1)
+//             return dp[i][j];
+
+//         if (s1[i - 1] == s2[j - 1])
+//             return find(i - 1, j - 1, s1, s2, dp);
+
+//         return dp[i][j] = 1 + min({find(i, j - 1, s1, s2, dp),
+//                                            find(i - 1, j, s1, s2, dp),
+//                                            find(i - 1, j - 1, s1, s2, dp)});
+//     }
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size();
+//         int m = word2.size();
+
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+//         for (int i = 1; i <= n; i++)
+//             dp[i][0] = i;
+//         for (int j = 1; j <= m; j++)
+//             dp[0][j] = j;
+//         return find(n, m, word1, word2, dp);
+//     }
+// };
+
+// tabulation with space optimization
 class Solution {
 public:
-    int find(int i, int j, string s1, string s2, vector<vector<int>>& dp) {
-        if (j == 0)
-            return i;
-        if (i == 0)
-            return j;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s1[i - 1] == s2[j - 1])
-            return find(i - 1, j - 1, s1, s2, dp);
-
-        return dp[i][j] = 1 + min({find(i, j - 1, s1, s2, dp),
-                                           find(i - 1, j, s1, s2, dp),
-                                           find(i - 1, j - 1, s1, s2, dp)});
-    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for (int i = 1; i <= n; i++)
-            dp[i][0] = i;
-        for (int j = 1; j <= m; j++)
-            dp[0][j] = j;
-        return find(n, m, word1, word2, dp);
+        vector<int> dp(m + 1);
+        for (int i = 0; i <= m; i++) dp[i] = i;
+
+        for (int i = 1; i <= n; i++) {
+            vector<int> new_dp(m + 1);
+            new_dp[0] = i;
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) new_dp[j] = dp[j-1];
+                else new_dp[j] = 1 + min({dp[j] , dp[j-1] , new_dp[j-1]});
+            }
+            dp = new_dp;
+        }
+
+        return dp[m];
     }
 };
