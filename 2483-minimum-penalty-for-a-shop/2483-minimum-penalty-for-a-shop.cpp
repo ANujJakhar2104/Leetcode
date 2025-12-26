ff8a4189ecb78e789e06a2669e3171f18b7ff499
ANujJakhar2104 <anuj.jakhar.2104@gmail.com>
2025-12-26 18:27:23 +0530

    Time: 15 ms (17.6%), Space: 21.6 MB (38.47%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -5,16 +5,16 @@ public:
         vector<int> temp_true(n, 0);
         vector<int> temp_false(n, 0);
 
-        for (int i = n - 1; i >= 0; i--) {
-            temp_true[i] = (customers[i] == 'Y');
-            if (i < n - 1)
-                temp_true[i] += temp_true[i + 1];
-        }
-
         for (int i = 0; i < n; i++) {
+            int j = n - 1 - i;
+
             temp_false[i] = (customers[i] == 'N');
             if (i > 0)
                 temp_false[i] += temp_false[i - 1];
+
+            temp_true[j] = (customers[j] == 'Y');
+            if (j < n - 1)
+                temp_true[j] += temp_true[j + 1];
         }
 
         int mini = temp_true[0];
@@ -22,8 +22,10 @@ public:
 
         for (int i = 1; i <= n; i++) {
             int curr = 0;
-            if (i < n) curr += temp_true[i];
-            if (i > 0) curr += temp_false[i - 1];
+            if (i < n)
+                curr += temp_true[i];
+            if (i > 0)
+                curr += temp_false[i - 1];
 
             if (curr < mini) {
                 mini = curr;
