364f0765143aa1b6a0ced91c6de51c8570f84c7d
ANujJakhar2104 <anuj.jakhar.2104@gmail.com>
2026-01-03 03:24:59 +0530

    Time: 11 ms (34.49%), Space: 17.9 MB (70.57%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int ans = 0;
+        int m= matrix.size();
+        int n= matrix[0].size();
+        vector<vector<pair<int,int>>> dp(m,vector<pair<int,int>>(n , {0,0}));
+
+        for(int i=0;i<n;i++){
+            if (matrix[0][i] == '1') {
+                dp[0][i].first =1;
+                dp[0][i].second = 1;
+            }
+            if (i > 0 && matrix[0][i-1] == '1'){
+                dp[0][i].second += dp[0][i-1].second;
+            }
+
+            ans = max(ans , dp[0][i].first * dp[0][i].second);
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (matrix[i][j] == '1'){
+                    dp[i][j].first = 1;
+                    dp[i][j].second = 1;
+
+                    if (matrix[i-1][j] == '1'){
+                        dp[i][j].first += dp[i- 1][j].first;
+                    }
+                    if (j>0 && matrix[i][j-1]=='1'){
+                        dp[i][j].second += dp[i][j-1].second;
+                    }
+                    int rows = dp[i][j].first;
+                    int idx = 0;
+                    int width = INT_MAX;
+                    while (idx!=rows){
+                        width = min(width , dp[i-idx][j].second);
+                        idx++;
+                        ans = max(ans , idx* width);
+                    }
+                    ans = max({ans,dp[i][j].first,dp[i][j].second});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
