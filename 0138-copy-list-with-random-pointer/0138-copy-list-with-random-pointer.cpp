89a46747bdae2c0fcc0e6edb76fac84a7f7b0fb3
Anuj Jakhar <anuj.jakhar.2104@gmail.com>
2026-08-13 21:46:16 +0530

    Time: 8 ms (27.63%), Space: 15.5 MB (8.55%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,37 +17,28 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* dup = head;
         unordered_map<Node* , Node*> mp;
-        unordered_map<Node* ,int> node_idx_original;
-        unordered_map<int , Node*> node_idx_new;
-        Node* temp = new Node(0);
-        Node* curr = temp;
+        unordered_map<Node* , Node*> new_to_old;
 
-        int cnt = 0;
-        while (dup){
-            Node* temp1 = new Node(dup->val);
-            curr->next = temp1;
-            curr = temp1;
-            node_idx_new[cnt] = temp1;
-            node_idx_original[dup] = cnt;
-            mp[dup] = dup->random;
-            cnt += 1;
-            dup = dup->next;
+        Node *new_head = new Node(0);
+        Node *temp = new_head;
+        Node *old_temp = head;
+
+        while(old_temp != nullptr){
+            Node *curr = new Node(old_temp->val);
+            new_to_old[curr] = old_temp;
+            mp[old_temp] = curr;
+            temp->next = curr;
+            temp = curr;
+            old_temp = old_temp->next;
         }
 
-        dup = head;
-        curr = temp->next;
-
-        while (dup && curr){
-            Node* rand = mp[dup];
-            if (rand != nullptr){
-                int idx = node_idx_original[rand];
-                curr->random = node_idx_new[idx];
-            }
-            dup = dup->next;
-            curr = curr->next;
+        temp = new_head->next;
+        while(temp != nullptr){
+            Node *new_random = new_to_old[temp]->random;
+            temp->random = mp[new_random];
+            temp = temp->next;
         }
-        return temp->next;
+        return new_head->next;
     }
 };
